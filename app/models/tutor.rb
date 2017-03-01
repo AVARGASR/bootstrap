fc7b934252609719c2627157b630b8d607ecc2fc
avargasr <avargasutem@gmail.com>
2017-03-01 17:01:43 -0300

    cambios

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,2 +1,15 @@
 class Tutor < ActiveRecord::Base
+
+	validates_format_of :nombre, :with => /\A[a-zA-Z\s]+\z/ , allow_blank: true
+	validates :nombre , :presence => {:message => "Campo obligatorio"}
+	validates_format_of :apellido, :with => /\A[a-zA-Z\s]+\z/ , allow_blank: true
+	validates :apellido , :presence => {:message => "Campo obligatorio"}
+	validates :rut, :uniqueness => {:message => "ya ingresado anteriormente"} , allow_blank: true
+	validates :rut, :presence => {:message => "Campo obligatorio"}
+	validates :rut, :rut => {:message => "no es valido"} , allow_blank: true
+	validates :email , :presence => {:message => "Campo obligatorio"}
+	validates :email, email_format: { message: "no es correcto" } , allow_blank: true
+	validates :asignatura, :presence => {:message => "tiene campo vacio"}
+	validates :carrera, :presence => {:message => "tiene campo vacio"}
+	validates :celu, length: { is: 9, :message => "Debe tener 9 digitos" }
 end
