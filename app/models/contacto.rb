2cbc955f868c598af83dbb2dc59fca888cbf928c
avargasr <avargasutem@gmail.com>
2017-03-01 15:53:26 -0300

    formularios

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,2 +1,8 @@
 class Contacto < ActiveRecord::Base
+
+	validates_format_of :nombre, :with => /\A[a-zA-Z\s]+\z/ , allow_blank: true
+	validates :nombre , :presence => {:message => "Campo obligatorio"}
+	validates :email , :presence => {:message => "Campo obligatorio"}
+	validates :email, email_format: { message: "no es correcto" } , allow_blank: true
+	validates :mensaje , :presence => {:message => "Campo obligatorio"}
 end
